cfbcff9028b017ee828a19b9699873de2e3b01a6
faizansiddiqui93 <145222759+faizansiddiqui93@users.noreply.github.com>
2023-11-02 20:57:41 +0530

    add new line

---
+++ shayan.c
@@ -0,0 +1,12 @@
+// code for the even  and odd no in c
+int main(){
+    int no;
+    printf("enter the no ");
+    scanf("%d",&no);
+    if(no%2==0){
+printf("the no is even");
+    }
+    else{
+        printf("no is odd");
+    }
+}
